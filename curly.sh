b3c64b0fb7ec9feb46c253eff457cadc18eda8d9
Jason Zhao <zhao473@purdue.edu>
2015-03-04 13:50:46 -0500

    Error fixing and adding some fp capability

--- curly.sh
+++ curly.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
-#Shell script that sends a Raspberry Pi's IP address to a specified server
+#Bash script that sends a Raspberry Pi's IP address to a specified server
 #Written by Jason S. Zhao on Feb 23, 2015
 
 IP="Timeout..."
-I=1000
+I=0.25
 
-while [$IP == "Timeout..." -a $I -lt 15000]
+#Fix comparison of fp nums
+while [ "$IP" == "Timeout..." -a $I -lt 15000 ]
 do
 	sleep $I
-	$I=$I*1.05
-	$IP=`ifconfig | sed -e '/wlan0/,/inet/!d'`
+	echo "$I"
+	I=$(expr $I*1.05 | bc)
+	IP=`ifconfig | sed -e '/eth0/,/inet/!d'`
 done
 
 #Store the IP in ip.txt and scp it to the specified server
 #Security is handled with a public ssh key
 #TODO: Setup SSH-KEY
-echo $IP > 'ip.txt'
+echo $IP > "ip.txt"
 scp ip.txt zhao473@data.cs.purdue.edu:~/
-
+echo "Done!"
