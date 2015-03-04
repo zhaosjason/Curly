8fe707b3d456cbc964d5bc221ef57bc90a489e15
Jason Zhao <zhao473@purdue.edu>
2015-03-03 23:33:29 -0500

    Just some incremental work to Curly

--- curly.sh
+++ curly.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 #Shell script that sends a Raspberry Pi's IP address to a specified server
-#regex for IP address from: http://www.cyberciti.biz/faq/grep-regular-expressions/
 #Written by Jason S. Zhao on Feb 23, 2015
 
 IP="Timeout..."
 I=1000
 
-while [$IP = "" -a $I -lt 180000]
+while [$IP == "Timeout..." -a $I -lt 15000]
 do
 	sleep $I
 	$I=$I*1.05
 	$IP=`ifconfig | sed -e '/wlan0/,/inet/!d'`
 done
 
-echo '$IP' > 'ip.txt'
-ftp jszhao@50.63.197.139
-sleep 1000
-echo "PASS"
-#curl jszhao @IP:DIR < IP.txt
+#Store the IP in ip.txt and scp it to the specified server
+#Security is handled with a public ssh key
+#TODO: Setup SSH-KEY
+echo $IP > 'ip.txt'
+scp ip.txt zhao473@data.cs.purdue.edu:~/
+
