81dfed996b33e068330188b3959806c3f7f3a859
Jason Zhao <zhao473@purdue.edu>
2015-03-04 22:47:38 -0500

    sleeping now supports fp numbers

--- curly.sh
+++ curly.sh
@@ -3,21 +3,27 @@
 #Bash script that sends a Raspberry Pi's IP address to a specified server
 #Written by Jason S. Zhao on Feb 23, 2015
 
-IP="Timeout..."
+IP=""
 I=0.25
 
 #Fix comparison of fp nums
-while [ "$IP" == "Timeout..." -a $I -lt 15000 ]
+while [ "$IP" == "" -a `echo $I"<"5000 | bc -l` ]
 do
 	sleep $I
-	echo "$I"
+	echo $I
 	I=$(expr $I*1.05 | bc)
-	IP=`ifconfig | sed -e '/eth0/,/inet/!d'`
+	IP=`ifconfig | sed -e '/wlandsf0/,/inet/!d'`
 done
 
 #Store the IP in ip.txt and scp it to the specified server
 #Security is handled with a public ssh key
 #TODO: Setup SSH-KEY
-echo $IP > "ip.txt"
-scp ip.txt zhao473@data.cs.purdue.edu:~/
+
+if [ "$IP" == "" ]
+then
+	echo Timeout...
+else
+	echo $IP > "ip.txt"
+	scp ip.txt zhao473@data.cs.purdue.edu:~/
+fi
 echo "Done!"
